94a6052fd55f6775cd80d75ccc386d1b91680386
mehmetgokhanagirgol <gokhanagirgol@gmail.com>
2022-06-06 18:20:08 +0300

    changed data structure to hold if a piece is underthreat added pawnthreat and inbounds functions

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 
 using namespace std;
 
-int checkPiece(char piece){
-    int piecePoint;
+struct box{
+    string piece;
+    bool underThreat;
+};
+
+bool inBounds(int i, int j){
+    return i >= 0 && i < 8 && j >= 0 && j < 8;
+}
+
+double checkPiece(char piece){
+    double piecePoint;
     switch (piece){
         case 'p':
             piecePoint = 1;
@@ -32,7 +41,63 @@ int checkPiece(char piece){
     return piecePoint;
 }
 
-void takeInput(vector<vector<string>> &board){
+double checkPawnThreat(int i, int j, int side, vector<vector<box>> &board){
+    // it will be easier to check pawn point by point since it has no piercing action
+    double piecePoint;
+    double penalty = 0;
+    if(side == 1){
+        if(inBounds(i+1, j+1) && board[i+1][j+1].piece[1] == 'b'){
+            if(!board[i+1][j+1].underThreat){
+                piecePoint = checkPiece(board[i+1][j+1].piece[0]);
+                penalty += piecePoint/2;
+                board[i+1][j+1].underThreat = true;
+            }
+        }
+        if(inBounds(i+1, j-1) && board[i+1][j-1].piece[1] == 'b'){
+            if(!board[i+1][j-1].underThreat){
+                piecePoint = checkPiece(board[i+1][j-1].piece[0]);
+                penalty += piecePoint/2;
+                board[i+1][j-1].underThreat = true;
+            }
+        }
+    }
+    else{
+        if(inBounds(i-1, j+1) && board[i-1][j+1].piece[1] == 's'){
+            if(!board[i-1][j+1].underThreat){
+                piecePoint = checkPiece(board[i-1][j+1].piece[0]);
+                penalty += piecePoint/2;
+                board[i-1][j+1].underThreat = true;
+            }
+        }
+        if(inBounds(i-1, j-1) && board[i-1][j-1].piece[1] == 's'){
+            if(!board[i-1][j-1].underThreat){
+                piecePoint = checkPiece(board[i-1][j-1].piece[0]);
+                penalty += piecePoint/2;
+                board[i-1][j-1].underThreat = true;
+            }
+        }
+    }
+    return penalty;
+}
+
+double checkKnightThreat(int i, int j, int side, vector<vector<box>> &board){
+    // it will be easier to check knight point by point since it has no piercing action
+    double piecePoint;
+    double penalty = 0;
+    if(side == 1){
+
+    }
+    else{
+
+    }
+    return 0;
+}
+
+double checkQueenThreat(int i, int j, int side, vector<vector<box>> &board){
+    return 0;
+}
+
+void takeInput(vector<vector<box>> &board){
     int row = 8;
     int col = 8;
     string piece;
@@ -40,23 +105,23 @@ void takeInput(vector<vector<string>> &board){
         board.push_back({});
         for(auto j = 0; j < col; ++j){
             cin >> piece;
-            board[i].push_back(piece);
+            board[i].push_back({piece, false});
         }
     }
 }
 
-pair<double, double> calculatePoints(vector<vector<string>> board){
+pair<double, double> calculatePoints(vector<vector<box>> board){
     double whitePoints = 0;
     double blackPoints = 0;
-    int piecePoint;
+    double piecePoint;
     for(auto i = 0; i < 8; ++i){
         for(auto j = 0; j < 8; ++j){
-            if(board[i][j][1] == 's'){
-                piecePoint = checkPiece(board[i][j][0]);
+            if(board[i][j].piece[1] == 's'){
+                piecePoint = checkPiece(board[i][j].piece[0]);
                 blackPoints += piecePoint;
             }
-            else if(board[i][j][1] == 'b'){
-                piecePoint = checkPiece(board[i][j][0]);
+            else if(board[i][j].piece[1] == 'b'){
+                piecePoint = checkPiece(board[i][j].piece[0]);
                 whitePoints += piecePoint;
             }
         }
@@ -64,14 +129,75 @@ pair<double, double> calculatePoints(vector<vector<string>> board){
     return make_pair(whitePoints, blackPoints);
 }
 
+double threatPenalty(int side, vector<vector<box>> &board){
+    // this function will only take care of queen, pawn and knight threats as indicated on the assignment mail
+    // white
+    char piece;
+    double penalty = 0;
+    if(side == 1){
+        // for every black queen, pawn and knight check radius if hit opponent increase the penalty by half value of the piece
+        for(auto i = 0; i < 8; ++i){
+            for(auto j = 0; j < 8; ++j){
+                if(board[i][j].piece[1] == 's'){
+                    piece = board[i][j].piece[0];
+                    switch (piece){
+                        case 'p':
+                            penalty += checkPawnThreat(i, j, 1, board);
+                            break;
+                        case 'a':
+                            penalty += checkKnightThreat(i, j, 1, board);
+                            break;
+                        case 'v':
+                            penalty += checkQueenThreat(i, j, 1, board);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    // black
+    else{
+        for(auto i = 0; i < 8; ++i){
+            for(auto j = 0; j < 8; ++j){
+                if(board[i][j].piece[1] == 'b'){
+                    piece = board[i][j].piece[0];
+                    switch (piece){
+                        case 'p':
+                            penalty += checkPawnThreat(i, j, 0, board);
+                            break;
+                        case 'a':
+                            penalty += checkKnightThreat(i, j, 0, board);
+                            break;
+                        case 'v':
+                            penalty += checkQueenThreat(i, j, 0, board);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    return penalty;
+}
+
 
 int main(){
-    vector<vector<string>> board;
+    vector<vector<box>> board;
     pair<double, double> points;
+    double threatPenaltyWhite;
+    double threatPenaltyBlack;
     // taking the input from the input.txt file
     takeInput(board);
     // calculate piece points for each player
     points = calculatePoints(board);
-    cout << points.first << " " << points.second << endl;
+    // substract threat penalties from each party
+    // I will use 0 to indicate white and 1 to indicate black
+    threatPenaltyWhite = threatPenalty(1, board);
+    threatPenaltyBlack = threatPenalty(0, board);
+    cout << points.first - threatPenaltyWhite << " " << points.second - threatPenaltyBlack << endl;
     return 0;
 }
